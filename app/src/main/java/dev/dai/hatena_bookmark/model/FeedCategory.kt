711e31426777fc5e562a8e31066c93c776b5c746
Dai1678 <dai7774@gmail.com>
2021-05-26 22:58:15 +0900

    FeedCategoryにメソッド追加

--- app/src/main/java/dev/dai/hatena_bookmark/model/FeedCategory.kt
+++ app/src/main/java/dev/dai/hatena_bookmark/model/FeedCategory.kt
@@ -1,8 +1,15 @@
 package dev.dai.hatena_bookmark.model
 
-enum class FeedCategory(name: String) {
+enum class FeedCategory(val categoryName: String) {
   ALL("総合"),
   SOCIAL("世の中"),
   ECONOMICS("政治と経済"),
-  LIFE("暮らし")
+  LIFE("暮らし");
+
+
+  companion object {
+    fun getCategoryNameList(): List<String> = values().map { it.categoryName }
+
+    fun fromOrdinal(ordinal: Int): FeedCategory = values().first { it.ordinal == ordinal }
+  }
 }
