0333af49894a86ddf0d4ca3961406b1af8f35c68
Dai1678 <dai7774@gmail.com>
2021-05-26 23:04:53 +0900

    TopAppBarとScrollableTabPagerを表示

--- app/src/main/java/dev/dai/hatena_bookmark/MainActivity.kt
+++ app/src/main/java/dev/dai/hatena_bookmark/MainActivity.kt
@@ -3,11 +3,15 @@ package dev.dai.hatena_bookmark
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import dev.dai.hatena_bookmark.model.FeedCategory
+import dev.dai.hatena_bookmark.ui.component.ScrollableTabPager
 import dev.dai.hatena_bookmark.ui.theme.HatenaBookmarkComposeSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +21,30 @@ class MainActivity : ComponentActivity() {
       HatenaBookmarkComposeSampleTheme {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
-          Greeting("Android")
+          AppContent()
         }
       }
+    }
+  }
+}
+
+@Composable
+private fun AppContent() {
+  Scaffold(
+    topBar = {
+      TopAppBar(
+        title = { Text(text = stringResource(id = R.string.app_name)) },
+        backgroundColor = MaterialTheme.colors.primary
+      )
+    },
+    modifier = Modifier.fillMaxSize()
+  ) {
+    ScrollableTabPager(pageTitleList = FeedCategory.getCategoryNameList()) { page ->
+      Text(
+        text = FeedCategory.fromOrdinal(page).categoryName,
+        modifier = Modifier.fillMaxSize(),
+        textAlign = TextAlign.Center
+      )
     }
   }
 }
