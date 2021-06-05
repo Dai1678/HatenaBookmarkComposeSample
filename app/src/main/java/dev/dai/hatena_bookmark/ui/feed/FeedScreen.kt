fcf19f9195adc7157fc64a4ea71282cdba24555e
Dai1678 <dai7774@gmail.com>
2021-06-05 20:58:22 +0900

    fix FeedViewModel

--- app/src/main/java/dev/dai/hatena_bookmark/ui/feed/FeedScreen.kt
+++ app/src/main/java/dev/dai/hatena_bookmark/ui/feed/FeedScreen.kt
@@ -4,22 +4,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
-import dev.dai.hatena_bookmark.model.FeedCategory
 
 @Composable
 fun FeedScreen(
-  feedCategory: FeedCategory,
+  feedCategoryName: String,
   feedViewModel: FeedViewModel = viewModel()
 ) {
   val uiState by feedViewModel.uiState.collectAsState()
-  feedViewModel.fetchFeedList(feedCategory)
+  feedViewModel.fetchFeedList(feedCategoryName)
 
   when {
     uiState.error != null -> {
     }
     else -> {
       FeedList(feedList = uiState.feedList, uiState.loading) {
-        feedViewModel.refresh(feedCategory)
+        feedViewModel.refresh(feedCategoryName)
       }
     }
   }
--- app/src/main/java/dev/dai/hatena_bookmark/ui/feed/FeedViewModel.kt
+++ app/src/main/java/dev/dai/hatena_bookmark/ui/feed/FeedViewModel.kt
@@ -3,10 +3,10 @@ package dev.dai.hatena_bookmark.ui.feed
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dev.dai.hatena_bookmark.model.FeedCategory
 import dev.dai.hatena_bookmark.repository.FeedRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,15 +17,16 @@ class FeedViewModel @Inject constructor(
   private val _uiState = MutableStateFlow(FeedUiState(loading = true))
   val uiState: StateFlow<FeedUiState> = _uiState
 
-  fun fetchFeedList(category: FeedCategory) {
+  fun fetchFeedList(categoryName: String) {
     viewModelScope.launch {
-      val feedList = feedRepository.feedList(category)
-      _uiState.value = FeedUiState(feedList = feedList)
+      feedRepository.feedList(categoryName).collect {
+        _uiState.value = FeedUiState(feedList = it)
+      }
     }
   }
 
-  fun refresh(category: FeedCategory) {
+  fun refresh(categoryName: String) {
     _uiState.value = _uiState.value.copy(loading = true)
-    fetchFeedList(category)
+    fetchFeedList(categoryName)
   }
 }
